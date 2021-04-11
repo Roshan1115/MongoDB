5afe955f0d10ffc5b387c0061a7ca9b8f79de69c
Roshan1115 <roshanbishi7890@gamil.com>
2021-04-11 18:15:26 +0530

    about Validators

--- Thapa-tutorial/Mongoose Tutorial/src/Validation.js
+++ Thapa-tutorial/Mongoose Tutorial/src/Validation.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const { default: validator } = require("validator")
 
 mongoose.connect("mongodb://localhost:27017/RoshanMONGOOSE",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("Connected Successfully............"))
@@ -6,7 +7,7 @@ mongoose.connect("mongodb://localhost:27017/RoshanMONGOOSE",{ useNewUrlParser: t
 
 
 // We neet to define the validation inside the schema
-const playlistSchema = new mongoose.Schema({
+const playlistSchema_2 = new mongoose.Schema({
   Name : {
       type: String,
 
@@ -23,12 +24,27 @@ const playlistSchema = new mongoose.Schema({
 
       // validation:- 
       require: true,
-      enum: ['Annie Marrie', 'Shelena']
+    //   enum: ['Annie Marrie', 'Shelena']
 
   },
   Duration : {
       type: Number,
-      require: true
+      //validation:-
+      require: true,
+    //   validate(val){
+    //       if(val <= 0){
+    //           throw new Error("Duration must be a positive.")
+    //       }
+    //   }
+  },
+  Singer_Email: {
+    type: String,
+    // validation:-
+    validate(val){
+        if( ! validator.isEmail(val)){
+            throw new Error("Please provide an appropriate email.")
+        }
+    }
   },
   Date : {
       type: Date,
@@ -37,10 +53,10 @@ const playlistSchema = new mongoose.Schema({
   Active : Boolean
 })
 
-const Playlist = new mongoose.model("Playlist", playlistSchema)
+const newPlaylist = new mongoose.model("Validation_Playlist", playlistSchema_2)
 
 //let try to insert document wtth validation applied
-const example_insert = new Playlist({
+const example_insert = new newPlaylist({
     // Name : 'BeautifuLLLLLLL',
     // Name : '    BeautifuLLLLLLL              '  ,
     // Name : 'BeautifuLLLLLLL',     //it will thrw err
@@ -54,3 +70,50 @@ const example_insert = new Playlist({
 // example_insert.save()
 // .then(() => console.log("saved"))
 // .catch(e => console.log(e))
+//............................................................
+
+
+
+
+
+// Custom validation
+// we gonna use same schema
+//for custom validation we have to weite a keywrd 'validate'
+//lets take the exaple of duration here
+const exm_inset_2 = async () => {
+    try{
+        const expSong = new newPlaylist({
+            Name: "Everybody Hates Me",
+            Singer: "The Chainsmoker",
+            Duration: -1.4,     //throw err
+            Active: true
+        })
+        const res = await expSong.save()
+        console.log(res);
+    }catch(err){
+        console.log(err);
+    }
+}
+// exm_inset_2();
+//..............................................................\\
+
+
+
+
+//npm validator (validator.js)
+const exm_inset_3 = async () => {
+    try{
+        const expSong = new newPlaylist({
+            Name: "The Nights",
+            Singer: "Avici",
+            Duration: 4.4, 
+            Singer_Email: "ads..pqr",   //throw err
+            Active: false
+        })
+        const res = await expSong.save()
+        console.log(res);
+    }catch(err){
+        console.log(err);
+    }
+}
+exm_inset_3();
